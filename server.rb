c4ea8a17702fc909f3a6af6e02c39b8df9b45816
Daniel Fone <daniel@fone.net.nz>
2024-11-11 22:36:25 +1300

    fixup! Nearly there

--- server.rb
+++ server.rb
@@ -57,7 +57,7 @@ end
 # This method is used to compare the user's input with the expected answer
 # It is case-insensitive and ignores non-word characters, i.e. punctuation
 def answer_matches?(actual, expected)
-  actual.downcase.gsub(/[^a-z]/, '') == expected.downcase.gsub(/[^a-z]/, '')
+  actual.downcase.gsub(/[^a-z1-9]/, '') == expected.downcase.gsub(/[^a-z1-9]/, '')
 end
 
 helpers do
