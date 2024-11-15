a47f28b9c8c164f4a2781ed71b4aa78116781c18
Daniel Fone <daniel@fone.net.nz>
2024-11-16 01:08:04 +1300

    Fibb

--- examples/fibb.rb
+++ examples/fibb.rb
@@ -4,4 +4,6 @@
 
 # }n?:y??}0==1%)n(trqs|n|{?yna.)4-q,4+q(;2**i*5=q;l=i|l|{pam.<$stup
 
-puts$<.map{|l|i=l.to_i;q=5*i**2;[q+4,q-4].any?{|n|Math.sqrt(n)%1==0}??y:?n}
+puts$<.to_a.join.split.map{|l|i=l.to_i;q=5*i**2;[q+4,q-4].any?{|n|Math.sqrt(n)%1==0}??y:?n}
+
+# }n?:y??}0==1%)n(trqs.htaM|n|{?yna.]4-q,4+q[;2**i*5=q;i_ot.l=i|l|{pam.tilps.nioj.a_ot.<$stup
