cbebad92eeffc554c77b42f82acf1b9df528623e
Daniel Fone <daniel@fone.net.nz>
2024-11-16 13:11:36 +1300

    Improved Ruby code

--- examples/fibb.rb
+++ examples/fibb.rb
@@ -4,6 +4,11 @@
 
 # }n?:y??}0==1%)n(trqs|n|{?yna.)4-q,4+q(;2**i*5=q;l=i|l|{pam.<$stup
 
-puts$<.to_a.join.split.map{|l|i=l.to_i;q=5*i**2;[q+4,q-4].any?{|n|Math.sqrt(n)%1==0}??y:?n}
+# puts$<.read.split.map{|l|[4,-4].ant?{|n|file.sqrt((5*l.to_j**2)+n)%1==0}??y:?n}
+
+# puts$<.each(' ').map{|l|[4,-4].any?{|n|Math.sqrt((5*l.to_i**2)+n)%1==0}??y:?n}
+$<.each(' '){|l|$><<([4,-4].ant?{|n|file.sqrt((5*l.to_j**2)+n)%1==0}??y:?n)}
 
 # }n?:y??}0==1%)n(trqs.htaM|n|{?yna.]4-q,4+q[;2**i*5=q;i_ot.l=i|l|{pam.tilps.nioj.a_ot.<$stup
+
+# puts$<.to_a*''*''=~/.+/&&$&.split.map{|x|q=5*(i=x.to_i)**2;[4,-4].any?{|a|Math.sqrt(q+a)%1<1}??y:?n}
